67f4051dd253ec8efae5de4eaf743ef86e01d441
LARISA-IOANA MOGA <larisa.moga@stud.ubbcluj.ro>
2020-12-17 09:17:24 +0200

    Add project files.

---
+++ Data/Moga_Larisa_Lab6Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Moga_Larisa_Lab6.Models;
+
+namespace Moga_Larisa_Lab6.Data
+{
+    public class Moga_Larisa_Lab6Context : DbContext
+    {
+        public Moga_Larisa_Lab6Context (DbContextOptions<Moga_Larisa_Lab6Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Moga_Larisa_Lab6.Models.Book> Book { get; set; }
+    }
+}
